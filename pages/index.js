dd981150ae536680c7a9ed3d3a0eb07cc7fee813
Ravi9140 <ravindra.patel@bluepineapple.io>
2023-06-05 16:37:55 +0530

    Project Completed

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,54 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
+import HeroBanner from "@/components/HeroBanner";
+import ProductCard from "@/components/ProductCard";
+import Wrapper from "@/components/Wrapper";
+import { fetchDataFromApi } from "@/utils/api";
+import { useEffect, useState } from "react";
+export default function Home({ products }) {
+  //When Pre rendering is not required, we use client side fetching
+  // const [data, setData] = useState(null);
 
-const inter = Inter({ subsets: ["latin"] });
+  // useEffect(() => {
+  //   fetchProducts();
+  // }, []);
 
-export default function Home() {
-  return <main>Home Page..</main>;
+  // const fetchProducts = async () => {
+  //   const { data } = await fetchDataFromApi("/api/products");
+  //   setData(data);
+  // };
+
+  return (
+    <main>
+      <HeroBanner />
+      <Wrapper>
+        {/* heading and paragaph start */}
+        <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            Cushioning for Your Miles
+          </div>
+          <div className="text-md md:text-xl">
+            A lightweight Nike ZoomX midsole is combined with increased stack
+            heights to help provide cushioning during extended stretches of
+            running.
+          </div>
+        </div>
+        {/* heading and paragaph end */}
+
+        {/* products grid start */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          {products?.data?.map((product) => (
+            <ProductCard key={product?.id} data={product} />
+          ))}
+        </div>
+        {/* products grid end */}
+      </Wrapper>
+    </main>
+  );
+}
+
+export async function getStaticProps() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+
+  return {
+    props: { products },
+  };
 }
